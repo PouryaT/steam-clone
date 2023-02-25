f3caa1b28976cb2308a5e08620b68b598460e43f
PouryaT <pourya.accont83@gmail.com>
2023-02-25 17:36:25 +0330

    added head dropdown + upgrade library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './header/header.component';
 
+import { NgxPopperModule } from 'ngx-popper';
+
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -19,7 +21,8 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    NgxPopperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
