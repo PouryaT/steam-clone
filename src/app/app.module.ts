2c57b63036fa39fd4c44439d2448d1a6c11e808f
PouryaT <pourya.accont83@gmail.com>
2023-02-26 19:49:07 +0330

    added Sign In page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,27 @@ import { HeaderComponent } from './header/header.component';
 
 import { NgxPopperModule } from 'ngx-popper';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    HeaderComponent
+    HeaderComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    NgxPopperModule
+    NgxPopperModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
